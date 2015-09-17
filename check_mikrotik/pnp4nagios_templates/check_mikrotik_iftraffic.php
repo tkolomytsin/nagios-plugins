73f102504d0eadb8d5ca0ed9d017698c70fdecd9
Tikhon Kolomytsin <tikhon@clearscale.net>
2015-09-17 22:12:43 +0600

    update check_mikrotik_iftraff pnp4nagios template

--- check_mikrotik/pnp4nagios_templates/check_mikrotik_iftraffic.php
+++ check_mikrotik/pnp4nagios_templates/check_mikrotik_iftraffic.php
@@ -6,9 +6,9 @@ $ds_name[1] = "Interface Traffic";
 
 $def[1] = rrd::def("var1", $RRDFILE[1], $DS[1]);
 $def[1] .= rrd::def("var2", $RRDFILE[2], $DS[2]);
-$def[1] .= rrd::line2("var1", "#00CC00", "In\t");
+$def[1] .= rrd::area("var1", "#00CC00", "In\t");
 $def[1] .= rrd::gprint("var1", array("LAST", "AVERAGE", "MAX"), "%.2lf %sb/s");
-$def[1] .= rrd::line2("var2", "#0066B3", "Out\t");
+$def[1] .= rrd::line1("var2", "#0066B3", "Out\t");
 $def[1] .= rrd::gprint("var2", array("LAST", "AVERAGE", "MAX"), "%.2lf %sb/s");
 
 ?>
